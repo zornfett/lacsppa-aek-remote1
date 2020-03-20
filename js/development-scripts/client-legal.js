ec2406e71fefbfe72fe737281ad584ad2b15b8f6
Rob B <zornfett@users.noreply.github.com>
2020-03-19 19:51:05 -0700

    bye unnecessary scripts

--- js/development-scripts/client-legal.js
+++
@@ -1,7 +0,0 @@
-
-// CLIENT > LEGAL XXXX view populate
-jQuery('.content-topnav .confidential, .content-topnav .profile-legal').on('click', function(e){
-	console.log('chunga')
-;	// e.stopPropagation();
-	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-legal-confidential.html');
-});
