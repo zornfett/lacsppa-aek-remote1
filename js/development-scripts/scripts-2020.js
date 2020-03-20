5e89523e2d5fad9e2e38ae301cc113de31e47bdc
Rob B <zornfett@users.noreply.github.com>
2020-03-20 01:00:39 -0700

    CLIENT > Profile > Legal >Voter built-out

--- js/development-scripts/scripts-2020.js
+++ js/development-scripts/scripts-2020.js
@@ -99,8 +99,13 @@ jQuery('.content-topnav .judicial').on('click', function(e){
 
 // CLIENT > LEGAL fair-hearing view populate
 jQuery('.content-topnav .fair-hearing').on('click', function(e){
-	console.log('chunga')
 ;	// e.stopPropagation();
 	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-legal-fair.html');
 });
 
+// CLIENT > LEGAL VOTER view populate
+jQuery('.content-topnav .voter').on('click', function(e){
+	console.log('chunga')
+;	// e.stopPropagation();
+	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-legal-voter.html');
+});
