c5ff05adad3eed4094b533200599f5c32ff436ca
Rob B <zornfett@users.noreply.github.com>
2020-03-19 21:34:22 -0700

    CLIENT > Profile > Legal>Info built-out

--- js/development-scripts/scripts-2020.js
+++ js/development-scripts/scripts-2020.js
@@ -79,12 +79,19 @@ jQuery('.content-topnav .copay').on('click', function(e){
 	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-financial-copay.html');
 });
 
-
 // CLIENT > PROFILE > LEGAL
 
 // CLIENT > LEGAL CONFIDENTIAL view populate
 jQuery('.content-topnav .confidential, .content-topnav .profile-legal').on('click', function(e){
-	console.log('chunga')
 ;	// e.stopPropagation();
 	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-legal-confidential.html');
 });
+
+// CLIENT > LEGAL INFO view populate
+jQuery('.content-topnav .legal-info').on('click', function(e){
+	console.log('chunga')
+;	// e.stopPropagation();
+	jQuery('#insert-view-content-here').load('/KEA_QA/content-views/client/profile-legal-info.html');
+});
+
+
